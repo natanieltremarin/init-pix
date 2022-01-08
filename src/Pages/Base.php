9cd192994a7cea2d470d07b6e4fa5bc3936dd6b2
Nataniel Tremarin <nataniel.tremarin@universo.univates.br>
2022-01-08 11:27:05 -0300

    ajustes responsivos

--- src/Pages/Base.php
+++ src/Pages/Base.php
@@ -61,18 +61,21 @@
                 </div>
                 <?php include 'Parts/Message.php' ?>
             </div>
+            <div class="row">
+                <div class="col-12 text-center" style="color: #EEEEEE;">
+                    <br>
+                    <p class="dev-by">
+                        Developed by Nataniel Tremarin
+                        <br>
+                        <a target="_blank" href="https://www.instagram.com/natanieltremarin" style="text-decoration: none; color: #FFFFFF">
+                            <i class="fab fa-instagram"></i>
+                            @natanieltremarin
+                        </a>
+                    </p> 
+                </div>
+            </div>
         </section>
     </main>
-    <footer>
-        <p class="dev-by">
-            Developed by Nataniel Tremarin
-            <br>
-            <a target="_blank" href="https://www.instagram.com/natanieltremarin" style="text-decoration: none; color: #FFFFFF">
-                <i class="fab fa-instagram"></i>
-                @natanieltremarin
-            </a>
-        </p> 
-    </footer>
     <script src="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/js/fontawesome.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.bundle.min.js"></script>
